334563774293a01ffe4fb75109b1bea188bcec7c
kuklin-roman@yandex.ru <Hjvfy30@ug>
2022-12-05 20:40:37 +0300

    4.2. Заголовок в файле java [#159036 #367942]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -3,5 +3,6 @@ package ru.job4j;
 public class Info {
     public static void main(String[] args){
         System.out.println("05/12/2022");
+        System.out.println("В IDEA ошибок нет");
     }
 }
