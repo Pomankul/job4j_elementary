076505fe58cb10ae680ba8d279fa0b7a82b48fcd
kuklin-roman@yandex.ru <Hjvfy30@ug>
2023-10-23 16:10:29 +0300

    9.2. Математические операторы и составные операторы присваивания [#504905]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -11,7 +11,7 @@ public class Fit {
 
     public static void main(String[] args) {
         double man = Fit.manWeight(178);
-        double woman = Fit.womanWeight(178);
+        double woman = Fit.womanWeight(175);
         System.out.println("Man 178 is " + man);
         System.out.println("Woman 175 is " + woman);
     }
