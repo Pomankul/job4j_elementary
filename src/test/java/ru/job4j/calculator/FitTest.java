becf1015cc6411aa16efb5df7db1c0ef9b3fda69
kuklin-roman@yandex.ru <Hjvfy30@ug>
2023-10-25 21:54:49 +0300

    9.2. Математические операторы и составные операторы присваивания [#504905]

---
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class FitTest {
+
+    @Test
+    void whenMan180Then92() {
+        short in = 180;
+        double expected = 92;
+        double out = Fit.manWeight(in);
+        double esp = 0.01;
+        assertThat(out).isEqualTo(expected, withPrecision(esp));
+    }
+
+    @Test
+    void whenWoman170Then69() {
+        short in = 170;
+        double expected = 69;
+        double out = Fit.womanWeight(in);
+        double esp = 0.01;
+        assertThat(out).isEqualTo(expected, withPrecision(esp));
+    }
+}
