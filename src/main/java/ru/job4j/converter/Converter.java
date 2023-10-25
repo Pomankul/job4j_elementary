95fb4811f48d893ad2a1db5a931f2608f2159241
kuklin-roman@yandex.ru <Hjvfy30@ug>
2023-10-25 13:31:46 +0300

    9.2. Математические операторы и составные операторы присваивания [#504905]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -10,6 +10,18 @@ public class Converter {
     }
 
     public static void main(String[] args) {
+        float inEuro = 140;
+        float expectedEuro = 2;
+        float outEuro = Converter.rubleToEuro(inEuro);
+        boolean passedEuro = expectedEuro == outEuro;
+        System.out.println("140 rubles are 2. Test result : " + passedEuro);
+
+        float inDol = 120;
+        float expectedDol = 2;
+        float outDol = Converter.rubleToDollar(inDol);
+        boolean passedDol = expectedDol == outDol;
+        System.out.println("120 rubles are 2. Test result : " + passedDol);
+
         float euro = Converter.rubleToEuro(70);
         float dollar = Converter.rubleToDollar(60);
         System.out.println("70 rubles are " + euro + " euro");
