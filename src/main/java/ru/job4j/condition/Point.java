019a05bccfe8059cf487a7e80efe61a786587866
kuklin-roman@yandex.ru <Hjvfy30@ug>
2023-10-25 22:35:49 +0300

    9.2. Математические операторы и составные операторы присваивания [#504905]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -6,9 +6,7 @@ public class Point {
     }
 
     public static void main(String[] args) {
-        double result1 = Point.distance(0, 0, 2, 0);
-        double result2 = Point.distance(3, 4, 5, 6);
-        System.out.println("result 1 (0, 0) to (2, 0) " + result1);
-        System.out.println("result 2 (3, 4) to (5, 6) " + result2);
+        double result = Point.distance(0, 0, 2, 0);
+        System.out.println("result (0, 0) to (2, 0) " + result);
     }
 }
---
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class PointTest {
+
+    @Test
+    void when00to20then2() {
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double expected = 2;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when34to56then2() {
+        int x1 = 3;
+        int y1 = 4;
+        int x2 = 5;
+        int y2 = 6;
+        double expected = 2.83;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when01to51then5() {
+        int x1 = 0;
+        int y1 = 1;
+        int x2 = 5;
+        int y2 = 1;
+        double expected = 5;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+}
