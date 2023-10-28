81d7f4565fc3d206d0eb5a2ddd2f835765dd41e0
kuklin-roman@yandex.ru <Hjvfy30@ug>
2023-10-28 10:56:55 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,6 +8,14 @@ public class ArrayDefinition {
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
-
+        String[] names = new String[4];
+        names[0] = "Рома";
+        names[1] = "Лида";
+        names[2] = "Алиса";
+        names[3] = "Лютик";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
